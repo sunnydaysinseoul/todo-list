85110718531b15de55675581b3632cca6be6499f
Youngsun <youngsunhan.kr@gmail.com>
2021-03-06 11:58:12 +0900

    Changed from 'title.classList.add +title.classList.remove' to 'title.classList.toggle

--- first_func.js
+++ first_func.js
@@ -2,14 +2,19 @@ const title = document.querySelector("#title");
 
 const CLICKED_CLASS = "clicked";
 
+// function handleClick() {
+//   const hasClass = title.classList.contains(CLICKED_CLASS);
+//   if (hasClass) {
+//     // title.className = CLICKED_CLASS; 이렇게하면 원래있던 class까지 다 지워버리게됨
+//     title.classList.add(CLICKED_CLASS);
+//   } else{
+//       title.classList.remove(CLICKED_CLASS); 
+//   }
+// }
+
 function handleClick() {
-  const hasClass = title.classList.contains(CLICKED_CLASS);
-  if (hasClass) {
-    // title.className = CLICKED_CLASS; 이렇게하면 원래있던 class까지 다 지워버리게됨
-    title.classList.add(CLICKED_CLASS);
-  } else{
-      title.classList.remove(CLICKED_CLASS); 
-  }
+  // if CLICKED_CLASS is set remove it, otherwise add it
+  title.classList.toggle(CLICKED_CLASS)
 }
 
 function init() {
